e03c4954b2d833bab5f1aba5b653687881e5236e
Your Name <you@example.com>
2026-07-05 16:14:33 +0800

    test

---
+++ chapter/myfirst.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Lenovo on 2026/7/5.
+//
+#include<iostream>
+
+int main() {
+    using namespace std;
+    cout<< "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret is!" << endl;
+    return 0;
+}
